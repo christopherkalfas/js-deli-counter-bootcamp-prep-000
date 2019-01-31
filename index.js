12ae07d199586a01ca778f055c67b62d47c0e68b
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-31 22:04:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ return "Welcome, " + newName + ". You are number " + katzDeli.length + " in line
 }
 
 function nowServing(katzDeli){
+  for (let i= 0; i<10; i++)
+  
+  
+  
   return katzDeli 
   katzDeli.shift()
 }

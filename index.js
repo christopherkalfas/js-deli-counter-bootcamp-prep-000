40f32b69aea87b5921cdb5f361a765a4be23e9fc
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-31 22:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ return "Welcome, " + newName + ". You are number " + katzDeli.length + " in line
 }
 
 function nowServing(katzDeli){
-  if (katzDeli === 0)
+  if (katzDeli === 0){
   return "There is nobody waiting to be served!";
-  } 
+                     } 
   else {
-    var newName = katzDeli{[0]
-    katzDeli.splice(0,1)
-    return "Now serving" + katzDeli
-  }
-  
-  
-  
-}
+    var newName = katzDeli{[0];
+    katzDeli.splice(0,1);
+    return "Now serving" + katzDeli;
+         }
+                              }
